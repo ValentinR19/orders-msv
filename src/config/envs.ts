c8ce42bbdece4c53742d3e9c109d0aa50649ddad
Valentin Rodriguez <vrodriguez.crucijuegos@gmail.com>
2024-05-10 15:49:26 -0300

    Se agrego docker

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,7 +3,6 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  DATABASE_URL: string;
   DB_HOST: string;
   DB_PORT: number;
   DB_USERNAME: string;
@@ -11,15 +10,12 @@ interface EnvVars {
   DB_LOGGING: string;
   DB_LOGGING_ENABLE: boolean;
   DB_NAME: string;
-  PRODUCT_MSV_HOST: string;
-  PRODUCT_MSV_PORT: number;
   NATS_SERVERS: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    DATABASE_URL: joi.string().required(),
     DB_NAME: joi.string().required(),
     DB_HOST: joi.string().required(),
     DB_PORT: joi.number().required(),
@@ -27,8 +23,6 @@ const envsSchema = joi
     DB_PASSWORD: joi.string().required(),
     DB_LOGGING: joi.string().required(),
     DB_LOGGING_ENABLED: joi.boolean().required(),
-    PRODUCT_MSV_HOST: joi.string().required(),
-    PRODUCT_MSV_PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string().required()),
   })
   .unknown(true);
@@ -46,7 +40,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  databaseUrl: envVars.DATABASE_URL,
   dbHost: envVars.DB_HOST,
   dbPort: envVars.DB_PORT,
   dbUsername: envVars.DB_USERNAME,
@@ -54,7 +47,5 @@ export const envs = {
   dbLogging: envVars.DB_LOGGING,
   dbLoggingEnable: envVars.DB_LOGGING_ENABLE,
   dbName: envVars.DB_NAME,
-  productMsvHost: envVars.PRODUCT_MSV_HOST,
-  productMsvPort: envVars.PRODUCT_MSV_PORT,
   natsServers: envVars.NATS_SERVERS,
 };
